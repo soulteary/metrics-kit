3f24228cb91687a88a15017082097373800830f5
Su Yang <soulteary@users.noreply.github.com>
2026-08-26 02:59:47 +0800

    feat!: migrate Fiber integration to v3

--- http.go
+++ http.go
@@ -4,8 +4,8 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/adaptor"
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/adaptor"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
